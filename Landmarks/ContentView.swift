a5e36351f5c3e871f53a56a16497716560cd3797
201411108 <aggsae@gmail.com>
2021-06-24 23:33:04 +0900

    구현: Section 3. 스택을 활용한 다중 뷰 구현
    body 변수 내에는 하나의 뷰만 포함될 수 있음
    여러 뷰를 표현할 때는 stack을 활용해야 함
    - VStack : vertical stack, 세로로 배치할 때 사용
    - HStack : horizontal stack, 가로로 배치할 때 사용
    - Spacer : 뷰의 고정된 길이가 아닌 포함되어 있는 부모 뷰의 모든 공간을
    활용
    - 코드로 작성해도 되고, stack에 넣기를 원하는 뷰를 cmd + click 후 embed
    in stack을 통해서 작성할 수 있음

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -7,23 +7,28 @@
 
 import SwiftUI
 
-// 컨텐츠와 레이아웃을 포함하고 묘사하는 구조체
-// UI를 변경하는 방법
-// 1. inspector(미리보기 창)에서 선택하는 방법
-//    - 원하는 구성 요소에 cmd + 클릭을 통해 속성을 변경할 수 있음
-//    - modifiers 메소드를 통해 SwiftUI를 커스터마이징 할 수 있음
-//    - modifiers 메소드가 각 속성에 맞는 뷰를 보여줌.
-//    - modifires는 체이닝 방식으로, 수직적으로 작성됨
-// Xcode가 변경된 점을 바로 업데이트하여 사용자에게 보여준다.
-// 2. 코드에서 UI 요소를 cmd + 클릭 -> show swiftui inspector를 통해 디자인을 수정할 수 있음
+// Section 3 : combine views using stacks
+// 한 개의 뷰만 body 변수에 포함되기 여러 뷰를 보여주기 위해서는 스택에 포함시켜야 함
+// 1. Text의 cmd + click -> embed in vstack을 통해 vstack 생성(virtical stack)
+// 2. HStack -> horizontal stack
+// 3. Spacer -> 설정된 컨첸츠의 크기 대신 부모 View의 모든 공간을 활용할 수 있도록 해준다.
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title) // modifiers
+        VStack(alignment: .leading) { // 정렬 방식 수정
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding() // 내부 padding 부여
     }
 }
 
-// view의 preview를 제공하는 구조체
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
