5806af11197221ca21a35325effc8031f9faf8af
201411108 <aggsae@gmail.com>
2021-07-26 23:41:20 +0900

    기타: SwiftUI Essentials 응용 toy project 초기화

---
+++ ToupInfo/ToupInfo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToupInfo
+//
+//  Created by 김한동 on 2021/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToupInfo/ToupInfo/ToupInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToupInfoApp.swift
+//  ToupInfo
+//
+//  Created by 김한동 on 2021/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ToupInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
