d221fef00a5bb6c863c198721d477e0501a6153f
201411108 <aggsae@gmail.com>
2021-07-28 23:24:44 +0900

    기타: ContentView에 작성한 view 반영

--- ToupInfo/ToupInfo/Views/ContentView.swift
+++ ToupInfo/ToupInfo/Views/ContentView.swift
@@ -9,14 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            GeoCodeRow(geoName: "노원구")
-            GeoCodeRow(geoName: "강남구")
-            GeoCodeRow(geoName: "강북구")
-            GeoCodeRow(geoName: "도봉구")
-            GeoCodeRow(geoName: "성북구")
-            GeoCodeRow(geoName: "동대문구")
-        }
+        GeoCodeList()
     }
 }
 
