45b22ad1542b4355f127a304590def22e1bd0945
201411108 <aggsae@gmail.com>
2021-07-28 23:49:28 +0900

    리팩토링: 공통적으로 사용되는 view 통합
    지역명 및 시군구명의 경우 이름만 표현되는 단순 row view를 갖는 list
    view이다.
    인자로 넘겨주는 row의 이름만 다르면 되기 때문에 지역명(geocode)와
    시군구명(district)의 row view를 구분하지 않고 RowView 하나로 통합하여
    사용하도록 코드를 작성하였다.

---
+++ ToupInfo/ToupInfo/Views/RowView.swift
@@ -0,0 +1,20 @@
+//
+//  RowView.swift
+//  ToupInfo
+//
+//  Created by 김한동 on 2021/07/28.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RowView_Previews: PreviewProvider {
+    static var previews: some View {
+        RowView()
+    }
+}
