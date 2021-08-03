429a6215e3c1be8e7e5e9f4b7ac290ecc0743766
201411108 <aggsae@gmail.com>
2021-08-04 00:09:17 +0900

    구현: 통합한 view 반영

--- ToupInfo/ToupInfo/Views/RowView.swift
+++ ToupInfo/ToupInfo/Views/RowView.swift
@@ -2,19 +2,28 @@
 //  RowView.swift
 //  ToupInfo
 //
+//  이 뷰는 지역명 및 시군구명 표시 시 선택할 List 화면 구성을 위한 보조 View이다.
+//
 //  Created by 김한동 on 2021/07/28.
 //
 
 import SwiftUI
 
 struct RowView: View {
+    var name: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Spacer()
+            Text(name)
+                .multilineTextAlignment(.center)
+            Spacer()
+        }
     }
 }
 
 struct RowView_Previews: PreviewProvider {
     static var previews: some View {
-        RowView()
+        RowView(name: geocodes[0].name)
     }
 }
